30d1256d65c7eed3b861002e493045de90f211c3
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2023-05-22 15:50:22 +0200

    swap

---
+++ swap.c
@@ -0,0 +1,14 @@
+#include "sort.h"
+
+/**
+ * swap - Swaps two elements in an array.
+ * @i:The first element.
+ * @j: The second element.
+ * Return: nothing.
+ */
+void swap(int* array, int i, int j)
+{
+	int temp = array[i];
+	array[i] = array[j];
+	array[j] = temp;
+}
