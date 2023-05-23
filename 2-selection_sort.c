0b1388b5c8e224aef82e75075cdfd3ba80434a2e
Wayzer2318 <arezki.ait.kettout@gmail.com>
2023-05-23 04:21:00 +0200

    adding bracket to if

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -14,7 +14,9 @@ void selection_sort(int *array, size_t size)
     min = i;
     for (j = i + 1; j < size; j++)
       if (array[j] < array[min])
+      {
         min = j;
+      }
         if (min != i)
           swap(&array[i], &array[min]);
   }
