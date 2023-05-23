419dc3c6056875be7b508a25d5066e6ff444a987
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2023-05-23 12:18:30 +0200

    fixed error bubble sort function

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,9 +11,11 @@ void bubble_sort(int *array, size_t size)
 {
 		size_t i, j;
 		int temp;
-		
+
 		if (array == NULL || size == 0)
-			        return;
+		{
+			return;
+		}
 
 		for (i = 0; i < size - 1; i++)
 		{
