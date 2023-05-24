ac3f33f3c86e6e42f876d761026d0db60c346429
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2023-05-24 20:15:50 +0200

    fixed error function insertion_sort_list

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -42,20 +42,13 @@ void insertion_sort_list(listint_t **list)
 
 		while (current->prev != NULL && current->n < current->prev->n)
 		{
-			if (current->next != NULL)
-				current->next->prev = current->prev;
-			current->prev->next = current->next;
-
-			current->next = current->prev;
-			current->prev = current->next->prev;
-			current->next->prev = current;
+			swap_node(current->prev, current);
 
 			if (current->prev == NULL)
 				*list = current;
-			else
-				current->prev->next = current;
 
-			print_list(*list); 
+			print_list(*list);
+
 			current = current->prev;
 		}
 
