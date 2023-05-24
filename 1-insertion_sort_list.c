d5d23708998b9ef7cb29388e07e722d6c5ceb57d
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2023-05-24 12:06:50 +0200

    fixed error function swap and insertion sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,30 +1,18 @@
 #include "sort.h"
 
 /**
- * swap_node - swap two nodes in a doubly linked list.
- * @head: address of the head of the linked list.
+ * swap_node - swap two nodes in a doubly linked lis
  * @a: address of the first node.
  * @b: address of the second node.
  *
  * Return: nothing.
  */
-void swap_node(node* head, node* a, node* b)
+void swap_node(listint_t *a, listint_t *b)
 {
-	node* temp_head = head;
-
 	if (a->prev != NULL)
-	{
 		a->prev->next = b;
-	}
-	else
-	{
-		head = b;
-	}
-
 	if (b->next != NULL)
-	{
 		b->next->prev = a;
-	}
 
 	a->next = b->next;
 	b->prev = a->prev;
@@ -40,36 +28,25 @@ void swap_node(node* head, node* a, node* b)
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *node, current;
-
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
-	{
-		        return;
-	}
-	listint_t *node = NULL;
-	*current = *list;
+		return;
+
+	listint_t *current = (*list)->next;
 
 	while (current != NULL)
 	{
-	listint_t *next = current->next;
+		listint_t *insert = current->prev;
+		listint_t *next = current->next;
 
+		while (insert != NULL && insert->n > current->n)
+		{
+			swap_node(insert, current);
+			if (insert->prev == NULL)
+				*list = insert;
+			print_list(*list);
+			insert = current->prev;
+		}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+		current = next;
+	}
+}
