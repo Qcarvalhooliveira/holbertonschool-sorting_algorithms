afdcfa2ca930973075b384e0b1b01b9d933a8283
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2023-05-24 18:28:37 +0200

    fixed error function selection_sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -7,22 +7,22 @@
 */
 void selection_sort(int *array, size_t size)
 {
-  size_t i, j, min, temp;
-  
-  for (i = 0; i < size - 1; i++)
-  {
-    min = i;
-    for (j = i + 1; j < size; j++)
-      if (array[j] < array[min])
-      {
-        min = j;
-      }
-        if (min != i)
+	size_t i, j, min, temp;
+
+	for (i = 0; i < size - 1; i++)
+	{
+	min = i;
+	for (j = i + 1; j < size; j++)
+	if (array[j] < array[min])
+	{
+	min = j;
+	}
+	if (min != i)
 	{
 		temp = array[min];
 		array[min] = array[i];
 		array[i] = temp;
 		print_array(array, size);
 	}
-  }
+	}
 }
